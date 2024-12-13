326cd5c4fcdcbe24ace1248466d7ab844373f215
DamnPat <patrickwuke@163.com>
2024-12-13 14:41:07 +0800

    git add more cases

--- src/qpyci/cli.py
+++ src/qpyci/cli.py
@@ -1,5 +1,5 @@
 import typer
-from qpyci.commands import generate_badge, clean, run_tests
+from qpyci.commands import generate_badge, clean, run_tests,run_commands_from_file
 
 app = typer.Typer()
 
@@ -12,9 +12,14 @@ def clean():
     clean()
 
 @app.command()
-def test(cov_target: str):
+def coverage(cov_target: str):
     """Run tests with coverage for the specified target."""
     run_tests(cov_target)
+
+@app.command()
+def run(file_path: str = typer.Option(..., help="Path to the file containing commands to run.")):
+    """Run commands from the specified file."""
+    run_commands_from_file(file_path)
 
 def main():
     app()
--- src/qpyci/commands.py
+++ src/qpyci/commands.py
@@ -8,4 +8,18 @@ def clean():
 
 def run_tests(cov_target: str):
     subprocess.run(['pytest', '--cov-report', 'term', f'--cov={cov_target}', 'tests/'])
+
+
+def run_commands_from_file(file_path:str):
+    """Run commands from a given file."""
+    with open(file_path, 'r') as file:
+        commands = file.readlines()
     
+    for command in commands:
+        command = command.strip()
+        if command:  # Ensure the command is not empty
+            try:
+                result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+                print(f"Output of '{command}': {result.stdout}")
+            except subprocess.CalledProcessError as e:
+                print(f"Error while executing '{command}': {e.stderr}")
